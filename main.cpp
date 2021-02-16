3459f82426db4f1cf8416f0fc5b93a1b33ae8c62
Orlyn Gerano <orlyn_gerano@yahoo.com>
2021-02-16 19:03:02 +0100

    Allow to set player name

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "Console.h"
+#include <iostream>
+#include <string>
 
 int
 main(int argc, const char* argv[])
 {
+  std::string playerName = "Anonymous";
 
-  Console console("Orlyn");
+  if (argc > 1 && argv[1][0] != '\0') {
+    playerName = argv[1];
+  }
+
+  Console console(playerName);
   console.play();
 
   return 0;
